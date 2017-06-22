aee9273a797b96e4300d28bac7ce3839f5963b16
kallivad <kallivad@gmail.com>
2017-06-22 16:20:50 +0700

    Changed PBAS settings for new video

--- checker.bat
+++ checker.bat
@@ -1,1 +1,3 @@
-checker.exe ./dataset/video.avi ./foreground/img_mask.avi
+#checker.exe ./dataset/video.avi ./foreground/img_mask.avi
+#checker.exe ./dataset/video.avi ../out1.avi
+checker.exe ../video_new/out1.avi img_mask.avi
